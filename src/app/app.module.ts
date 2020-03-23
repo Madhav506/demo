a004da7498ff3ab804949927ccae59833ffa22f4
Madhav506 <madhavipasupuleti506@gmail.com>
2020-03-23 15:14:32 +0530

    3 files modified, 12 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,15 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
+import { HeaderComponent } from './header/header.component';
+import { CartIconComponent } from './cart-icon/cart-icon.component';
+import { SortComponent } from './sort/sort.component';
+import { FilterComponent } from './filter/filter.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent, SearchComponent ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, SearchComponent, HeaderComponent, CartIconComponent, SortComponent, FilterComponent, ShoppingListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
